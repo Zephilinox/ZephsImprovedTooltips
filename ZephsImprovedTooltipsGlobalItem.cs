2c0fed80f93fca7312d5086815cbe7da922e2885
Zephilinox <Zephilinox@hotmail.co.uk>
2019-07-04 23:13:58 +0100

    v0.11

--- ZephsImprovedTooltipsGlobalItem.cs
+++ ZephsImprovedTooltipsGlobalItem.cs
@@ -14,6 +14,11 @@ namespace ZephsImprovedTooltipsGlobalItem
 
         public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
         {
+            const bool useColour = false;
+            const string startColour = useColour ? "[c/ffc055:" : "";
+            const string endColour = useColour ? "]" : "";
+            const bool colourAllPercentages = false;
+
             bool firesProjectile = false;
             if (item.melee &&
                 item.shoot > 0 &&
@@ -24,7 +29,16 @@ namespace ZephsImprovedTooltipsGlobalItem
                 firesProjectile = true;
             }
 
-            int speed = (firesProjectile ? item.useAnimation : item.useTime);
+            int speed;
+            if (firesProjectile || 
+                item.useAnimation <= 0) //Fix wooden sword/light bane/etc
+            {
+                speed = item.useTime;
+            }
+            else
+            {
+                speed = item.useAnimation;
+            }
 
             //The crit takes in to account the currently held item, but we only care about the item we're moused over
             //Therefore, get the difference between the two and then apply it to the player's total crit later
@@ -73,31 +87,109 @@ namespace ZephsImprovedTooltipsGlobalItem
             for (int i = 0; i < tooltips.Count; ++i)
             {
                 TooltipLine line = tooltips[i];
-
-                if (line.Name == "Speed")
+                Main.NewText(line.Name);
+                if (useColour && (line.Name == "Damage" ||
+                    line.Name == "CritChance" ||
+                    line.Name == "PickPower" ||
+                    line.Name == "AxePower" ||
+                    line.Name == "HammerPower" ||
+                    line.Name == "Defense"))
                 {
-                    line.text += " (" + attacksPerSecond.ToString("0.#") + " times per second)";
+                    int spaceIndex = line.text.IndexOf(' ');
+                    if (spaceIndex >= 0)
+                    {
+                        line.text = startColour + line.text.Substring(0, spaceIndex) +  endColour + line.text.Substring(spaceIndex, line.text.Length - spaceIndex);
+                    }
+                }
+                else if (line.Name == "Speed")
+                {
+                    //todo: come up with my own names rather than use vanilla, faster and more accurate
+                    line.text = startColour + attacksPerSecond.ToString("0.#") + endColour + " attacks per second (" + line.text.Substring(0, line.text.Length - 6) + ")"; //5 = 5 in speed + space
 
+                    TooltipLine dpsLine = new TooltipLine(mod, "DPS", startColour + totalDPS + endColour + " damage per second");
                     if (dpsCrit > 0)
                     {
-                        TooltipLine dpsLine = new TooltipLine(mod, "DPS", totalDPS + " damage per second" + " (" + dpsCrit + " from crits)");
-                        tooltips.Insert(i + 1, dpsLine);
-                        i++;
+                        dpsLine.text += " (" + dpsCrit + " from crits)";
                     }
-                    else
-                    {
-                        TooltipLine dpsLine = new TooltipLine(mod, "DPS", dps + " damage per second");
-                        tooltips.Insert(i + 1, dpsLine);
-                        i++;
-                    }
+                    tooltips.Insert(i + 1, dpsLine);
+                    i++;
                 }
                 else if (line.Name == "Knockback")
                 {
                     if (item.knockBack > 0)
                     {
-                        line.text += " (" + item.knockBack + ")";
+                        line.text = startColour + item.knockBack.ToString("0.#") + endColour + " knockback (" + line.text.Substring(0, line.text.Length - 10) + ")"; //10 = 9 characters in knockback + space
                     }
-                }                
+                }
+                else if (!line.isModifier &&
+                    !line.isModifierBad &&
+                    useColour &&
+                    colourAllPercentages)
+                {
+                    int numberStartIndex = -1;
+                    int numberEndIndex = -1;
+                    bool percentFound = false;
+
+                    for (int charIndex = 0; charIndex < line.text.Length; ++charIndex)
+                    {
+                        if (line.text[charIndex] == '[' ||
+                            line.text[charIndex] == ']')
+                        {
+                            //someone is using custom thingies, ignore it
+                            percentFound = false;
+                            numberStartIndex = -1;
+                            numberEndIndex = -1;
+                            continue;
+                        }
+
+                        if (numberStartIndex < 0)
+                        {
+                            if (line.text[charIndex] >= '0' || line.text[charIndex] <= '9')
+                            {
+                                numberStartIndex = charIndex;
+                            }
+                        }
+                        else if (numberEndIndex < 0)
+                        {
+                            if (line.text[charIndex] < '0' || line.text[charIndex] > '9')
+                            {
+                                numberEndIndex = charIndex;
+
+                                if (line.text[charIndex] == '%')
+                                {
+                                    if (charIndex == line.text.Length - 1 ||
+                                        line.text[charIndex + 1] == ' ' || //there's a space after the %
+                                        line.text[charIndex + 1] == '.' || //. after %
+                                        line.text[charIndex + 1] == ',' || //etc
+                                        line.text[charIndex + 1] == '\n')
+                                    {
+                                        percentFound = true;
+                                    }
+                                }
+                            }
+                        }
+
+                        if (numberEndIndex >= 0) //this character turned out to be the end
+                        {
+                            if (percentFound)
+                            {
+                                line.text = line.text.Insert(numberStartIndex, "[c/ffc055:");
+                                if (numberEndIndex == line.text.Length - 1) //no more characters, so append one
+                                {
+                                    line.text += "]";
+                                }
+                                else
+                                {
+                                    line.text = line.text.Insert(numberEndIndex + 10 + 1, "]"); //we just added 10 chars, +1 to grab %
+                                }
+                                charIndex += 11; //length of chars we adding
+                            }
+                            percentFound = false;
+                            numberStartIndex = -1;
+                            numberEndIndex = -1;
+                        }
+                    }
+                }
             };
         }
     }
